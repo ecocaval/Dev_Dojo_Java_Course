833f63ebdf5dc11e890ef04b055ad4f7ca935ee7
erico <ecocaval@hotmail.com>
2023-04-17 10:26:25 -0300

    Wrappers studies

---
+++ src/aula106Wrappers/test/WrapperTest01.java
@@ -0,0 +1,33 @@
+package aula106Wrappers.test;
+
+public class WrapperTest01 {
+
+    public static void main(String[] args) {
+        byte byteTest = 1;
+        short shortTest = 1;
+        int intTest = 1;
+        long longTest = 1L;
+        float floatTest = 1F;
+        double doubleTest = 1D;
+        char charTest = '1';
+        boolean booleanTest = true;
+        String stringTest = "1";
+
+        Byte byteWrapper = byteTest;
+        Short shortWrapper = shortTest;
+
+        Byte byteWrapper2 = 1;
+        Short shortWrapper2 = 1;
+        Integer intWrapper = 1;
+        Long longWrapper = 1L;
+        Float floatWrapper = 1F;
+        Double doubleWrapper = 1D;
+        Character charWrapper = '1';
+        Boolean booleanWrapper = true;
+        String stringWrapper = "1";
+
+
+        byte byteTest2 = byteWrapper2;
+        short shortTest2 = shortWrapper2;
+    }
+}
